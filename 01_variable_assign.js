8b92d46da8b19db82612237b1cee7689dc25b4fe
kuldeepnawale777 <kuldeepnawale777@gmail.com>
2023-02-16 09:45:24 +0530

    Create Variable assign
    variable declaration ,Initialization, Update.

---
+++ 01_variable_assign.js
@@ -0,0 +1,30 @@
+var myFullName=
+console.log("-----Step :01------" );
+console.log(myFullName);
+myFullName="Kuldeep Nawale";
+console.log(myFullName);
+myFullName="Kuldeep Bhimrao Nawale";
+console.log(myFullName);
+
+console.log("-----Step :02------" );
+
+var myLove="I love only JS";
+console.log(myLove );
+
+console.log("-----Step :03------" );
+var firstName="Kuldeep";
+var lastName="Nawale";
+var age="25";
+var address="Kalyani Nagar Pune";
+var pincode="411014";
+var gender="Male";
+var isMarried="NO"
+
+console.log("First Name  :",firstName);
+console.log("Last  Name  :",lastName);
+console.log("Age         :",age);
+console.log("Address     :",address);
+console.log("pincode     :",pincode);
+console.log("Gender      :",gender);
+console.log("Is Married  :",isMarried);
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
 console.log("Kuldeep Nawale");
+console.log("25");
+console.log("Pune");
+
+var fullName = "Kuldeep Nawale"
+console.log(fullName)
+
+
+var city;
+console.log(city);
+
